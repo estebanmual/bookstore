2779c1d5b2fde31ffb09e3bd7662e94234548b7e
Estebanmual <esteban.munozal@gmail.com>
2022-03-28 13:59:10 -0600

    Added a key to the reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,7 +2,7 @@ import { combineReducers, createStore } from 'redux';
 import booksReducer from './books/books';
 import categoriesReducer from './categories/categories';
 
-const rootReducer = combineReducers({ booksReducer, categoriesReducer });
+const rootReducer = combineReducers({ books: booksReducer, categories: categoriesReducer });
 const store = createStore(rootReducer);
 
 export default store;
